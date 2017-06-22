ec39323fa4dec07b55b0fdb237a3efc3d2be80f0
SustainRelease <sirrobbiemuir@hotmail.com>
2017-06-22 13:31:30 -0500

    Fixed up contact page in mobile view

--- public/javascripts/designerFormat.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(() => {
-  $("img.designer").each(function() {
-    if ($(this).width() > 1.2*$(this).height()) {
-      $(this).addClass("wide");
-      $(this).parent().addClass("short");
-    }
-  });
-})
